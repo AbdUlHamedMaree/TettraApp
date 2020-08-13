25a10662e66ce7cd2d2e37483770e04a465d4f31
PC-ROG\ASUS <hmidmrii@gmail.com>
2020-08-13 21:38:27 +0300

    Oh...How I forgrt this

---
+++ wwwroot/dist/precache-manifest.9e09c2863f8d301a847906ea9ef2e0ba.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a23315cf1a3048de74eba8293849572",
+    "url": "/dist/index.html"
+  },
+  {
+    "revision": "2845ab5bbe85278af208",
+    "url": "/dist/static/css/bundle.css"
+  },
+  {
+    "revision": "2845ab5bbe85278af208",
+    "url": "/dist/static/js/bundle.js"
+  },
+  {
+    "revision": "b9b0c4bcf069b3796814e85ceb15f57e",
+    "url": "/dist/static/js/bundle.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d9d82e797fbcbf34101612c24b3aa05",
+    "url": "/dist/static/media/Profile.4d9d82e7.jpg"
+  }
+]);
